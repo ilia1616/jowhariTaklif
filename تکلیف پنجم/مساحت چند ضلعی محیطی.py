c8e57f4c1332436b1895aa88ab0a18bcd31bb91a
seyed ali <miriali946@gmail.com>
2024-12-18 13:34:16 +0330

    sorted X and Y

--- تکلیف پنجم/مساحت چند ضلعی محیطی.py
+++ تکلیف پنجم/مساحت چند ضلعی محیطی.py
@@ -4,3 +4,10 @@ points = []
 for i in x:
     points.append(i.split(','))
 
+def m(Z):
+    return Z[1]
+
+X = sorted(points) # مرتب کردن بر اساس طول
+Y = sorted(points, key=m)
+# pointsY = sorted(points, key=lambda x: x[1])
+
