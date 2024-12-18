bf5502cb6b542d0194aeb11603c360809c19cefa
seyed ali <miriali946@gmail.com>
2024-12-18 12:25:18 +0330

    considered if line fragment is vertical

--- تکلیف پنجم/ایا همدیگر را قطع میکنند.py
+++ تکلیف پنجم/ایا همدیگر را قطع میکنند.py
@@ -11,16 +11,20 @@ for i in x1:
     second_line.append(i.split(','))
 second_line = sorted(second_line)
     
-   
-m0 = (float(first_line[1][1]) - float(first_line[0][1])) / (float(first_line[1][0]) - float(first_line[0][0])) # شیب پاره خط 1
-m1 = (float(second_line[1][1]) - float(second_line[0][1])) / (float(second_line[1][0]) - float(second_line[0][0])) # شیب پاره خط 2
 
+if (float(first_line[1][0]) - float(first_line[0][0])) != 0 and (float(second_line[1][0]) - float(second_line[0][0])) != 0:
+    m0 = (float(first_line[1][1]) - float(first_line[0][1])) / (float(first_line[1][0]) - float(first_line[0][0])) # شیب پاره خط 1
+    m1 = (float(second_line[1][1]) - float(second_line[0][1])) / (float(second_line[1][0]) - float(second_line[0][0])) # شیب پاره خط 2
+else:
+    if second_line[0][0] <= first_line[0][0] <= second_line[1][0] or first_line[0][0] <= second_line[0][0] <= first_line[1][0]:
+        print('yes')
+    
 # x = (- m0x0 + y0 + m1x1 - y1) / (m1 -m0)
 # فرمول محاسبه طول نقطه ی برخورد
 
 if m1 != m0:
     X = (-m0 * float(first_line[0][0]) + float(first_line[0][1]) + m1 * float(second_line[0][0]) - float(second_line[0][1])) / (m1 - m0)
-elif m1 == m0:
+else:
     print('no')
     
 if X <= float(first_line[1][0]) and X >= float(first_line[0][0]) and X <= float(second_line[1][0]) and X >= float(second_line[0][0]):
